6a4f8d9f524d6f3bc68fc4fdaaff4bf233d62adf
Yo-Ma <yoma@netc.fr>
2018-03-19 21:45:03 +0100

    first commit

---
+++ src/main/java/jeu/Guerrier.java
@@ -0,0 +1,11 @@
+package jeu;
+
+public class Guerrier {
+
+    String name = null;
+    String image = null;
+    int life = 60;                          // Value between 0 and 100
+    int attack = 25;                        // Value between 5 and 50
+    String tableWeapon[] = { };             // Can have one or more
+    String shield = null;
+}
---
+++ src/main/java/jeu/Magicien.java
@@ -0,0 +1,11 @@
+package jeu;
+
+public class Magicien {
+
+    String name = null;
+    String image = null;
+    int life = 45;                  // Value between 0 and 100
+    int attack = 15;                // Value between 10 and 50
+    String tableThreat[] = { };            // Can have one or more
+    String filter = null;
+}
