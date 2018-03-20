4712310dfd93fd447cf9bec8dccc96b7b05b0a4c
Yo-Ma <yoma@netc.fr>
2018-03-20 12:09:36 +0100

    Change arrays syntaxe from 'type name[]' to 'type[] name'

--- src/main/java/jeu/Guerrier.java
+++ src/main/java/jeu/Guerrier.java
@@ -6,6 +6,6 @@ public class Guerrier {
     String image = null;
     int life = 60;                          // Value between 0 and 100
     int attack = 25;                        // Value between 5 and 50
-    String tableWeapon[] = { };             // Can have one or more
+    String[] tableWeapon = { };             // Can have one or more
     String shield = null;
 }
--- src/main/java/jeu/Magicien.java
+++ src/main/java/jeu/Magicien.java
@@ -6,6 +6,6 @@ public class Magicien {
     String image = null;
     int life = 45;                  // Value between 0 and 100
     int attack = 15;                // Value between 10 and 50
-    String tableThreat[] = { };            // Can have one or more
+    String[] tableThreat = { };            // Can have one or more
     String filter = null;
 }
