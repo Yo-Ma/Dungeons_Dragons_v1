8f0311f3d871ddeb62bfbfc96d3815ffcabf137c
Yo-Ma <yoma@netc.fr>
2018-03-21 09:35:49 +0100

    few changes : formatting code

--- src/main/java/jeu/Guerrier.java
+++ src/main/java/jeu/Guerrier.java
@@ -2,10 +2,10 @@ package jeu;
 
 public class Guerrier {
 
-    String name = null;
-    String image = null;
-    int life = 60;                          // Value between 0 and 100
-    int attack = 25;                        // Value between 5 and 50
-    String[] tableWeapon = { };             // Can have one or more
-    String shield = null;
+    String   name        = null;
+    String   image       = null;
+    int      life        = 60;                      // Value between 0 and 100
+    int      attack      = 25;                      // Value between 5 and 50
+    String[] tableWeapon = {};                      // Can have one or more
+    String   shield      = null;
 }
--- src/main/java/jeu/Magicien.java
+++ src/main/java/jeu/Magicien.java
@@ -2,10 +2,10 @@ package jeu;
 
 public class Magicien {
 
-    String name = null;
-    String image = null;
-    int life = 45;                  // Value between 0 and 100
-    int attack = 15;                // Value between 10 and 50
-    String[] tableThreat = { };            // Can have one or more
-    String filter = null;
+    String   name        = null;
+    String   image       = null;
+    int      life        = 45;                  // Value between 0 and 100
+    int      attack      = 15;                  // Value between 10 and 50
+    String[] tableThreat = {};                  // Can have one or more
+    String   filter      = null;
 }
